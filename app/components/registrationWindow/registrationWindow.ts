f32e4b9a80be6c5c8ab70434098a2055bfd91f74
Mr.Anderson <tonibuzov2011@gmail.com>
2017-08-23 18:52:20 +0200

    registration

--- app/components/registrationWindow/registrationWindow.ts
+++ app/components/registrationWindow/registrationWindow.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentChecked } from '@angular/core';
+
+import UserService from '../../services/UserService';
 
 @Component({
     selector: "registration-window", 
@@ -21,49 +23,108 @@ import { Component } from '@angular/core';
 	                            </div>
 	                            <div class="form-bottom">
 				                    <form role="form" action="" method="post" class="registration-form">
-				                    	<div class="form-group">
-				                    		<label class="sr-only" for="form-first-name">First name</label>
-				                        	<input type="text" name="form-first-name" placeholder="First name..." class="form-first-name form-control" id="form-first-name">
-				                        </div>
-				                        <div class="form-group">
-				                        	<label class="sr-only" for="form-last-name">Last name</label>
-				                        	<input type="text" name="form-last-name" placeholder="Last name..." class="form-last-name form-control" id="form-last-name">
-				                        </div>
-                                        <div class="form-group">
-				                    		<label class="sr-only" for="form-username">Username</label>
-				                        	<input type="text" name="form-user-name" placeholder="User name..." class="form-user-name form-control" id="form-user-name">
-				                        </div>
-                                        <div class="form-group">
-				                    		<label class="sr-only" for="form-town">Town</label>
-				                        	<input type="text" name="form-town" placeholder="Your town..." class="form-town form-control" id="form-town">
-				                        </div>
-                                        <div class="form-group">
-				                    		<label class="sr-only" for="form-country">Country</label>
-				                        	<input type="text" name="form-country" placeholder="Your country..." class="form-country form-control" id="form-country">
-				                        </div>
-                                        <div class="form-group">
-                                            <label class="sr-only" for="form-data">DATE</label>
-                                            <input class="form-control" name="form-data" type="date" placeholder="Data..." id="date-input">
 
+				                    	<div [ngClass]="{'form-group':true,'has-error has-feedback': firstNameWarning}">
+				                    		<label class="sr-only" for="form-first-name">First name</label>
+				                        	<input type="text" #firstName name="form-first-name" placeholder="First name..." class="form-first-name form-control" id="form-first-name">
+                                            <span *ngIf="firstNameWarning" class="glyphicon glyphicon-remove form-control-feedback"></span>
+                                             <span class="control-label" *ngIf="firstNameWarning">
+                                              You need to fill up this form
+                                            </span>
+				                        </div>
+
+				                        <div [ngClass]="{'form-group':true,'has-error has-feedback': lastNameWarning}">
+				                        	<label class="sr-only" for="form-last-name">Last name</label>
+				                        	<input type="text" #lastName name="form-last-name" placeholder="Last name..." class="form-last-name form-control" id="form-last-name">
+                                            <span *ngIf="lastNameWarning" class="glyphicon glyphicon-remove form-control-feedback"></span>
+                                             <span class="control-label" *ngIf="lastNameWarning">
+                                              You need to fill up this form
+                                            </span>
+				                        </div>
+
+                                        <div [ngClass]="{'form-group':true,'has-error has-feedback': usernameWarning || userAlreadyExistWarning}">
+				                    		<label class="sr-only" for="form-username">Username</label>
+				                        	<input type="text" #username name="form-user-name" placeholder="User name..." class="form-user-name form-control" id="form-user-name">
+                                            <span *ngIf="usernameWarning" class="glyphicon glyphicon-remove form-control-feedback"></span>
+                                             <span class="control-label" *ngIf="usernameWarning">
+                                              You need to fill up this form
+                                            </span>
+                                            <span class="control-label" *ngIf="userAlreadyExistWarning">
+                                             User already exist, please change user name
+                                            </span>
+				                        </div>
+
+
+                                        <div [ngClass]="{'form-group':true,'has-error has-feedback': cityWarning}">
+				                    		<label class="sr-only" for="form-city">City</label>
+				                        	<input type="text" #city name="form-city" placeholder="Your city..." class="form-city form-control" id="form-city">
+                                            <span *ngIf="cityWarning" class="glyphicon glyphicon-remove form-control-feedback"></span>
+                                            <span class="control-label" *ngIf="cityWarning">
+                                              You need to fill up this form
+
+                                            </span>
+				                        </div>
+
+                                        <div [ngClass]="{'form-group':true,'has-error has-feedback': countryWarning}">
+				                    		<label class="sr-only" for="form-country">Country</label>
+				                        	<input type="text" #country name="form-country" placeholder="Your country..." class="form-country form-control" id="form-country">
+                                            <span *ngIf="countryWarning" class="glyphicon glyphicon-remove form-control-feedback"></span>
+                                               <span class="control-label" *ngIf="countryWarning">
+                                              You need to fill up this form
+
+                                            </span>      
+				                        </div>
+
+                                        <div [ngClass]="{'form-group':true,'has-error has-feedback': dateWarning}">
+                                            <label class="sr-only" for="form-data">DATE</label>
+                                            <input class="form-control" #date name="form-date" type="date" placeholder="Date..." id="date-input">
+                                            <span *ngIf="dateWarning" class="glyphicon glyphicon-remove form-control-feedback"></span>
+                                             <span class="control-label" *ngIf="dateWarning">
+                                             You need to fill up this form
+
+                                            </span>
                                         </div>
-				                        <div class="form-group">
+				                        <div [ngClass]="{'form-group':true,'has-error has-feedback': emailWarning}">
 				                        	<label class="sr-only" for="form-email">Email</label>
-				                        	<input type="email" name="form-email" placeholder="Email..." class="form-email form-control" id="form-email">
+				                        	<input type="email" #email name="form-email" placeholder="Email..." class="form-email form-control" id="form-email">
+                                            <span *ngIf="emailWarning" class="glyphicon glyphicon-remove form-control-feedback"></span>
+                                             <span class="control-label" *ngIf="emailWarning">
+                                            You need to fill up this form
+                                            </span>
 				                        </div>
-				                        <div class="form-group">
+
+				                        <div [ngClass]="{'form-group':true,'has-error has-feedback': abutMeWarning}">
 				                        	<label class="sr-only" for="form-about-yourself">About yourself</label>
-				                        	<textarea name="form-about-yourself" placeholder="About yourself..." 
+				                        	<textarea #aboutMe name="form-about-yourself" placeholder="About yourself..." 
 				                        	class="form-about-yourself form-control" id="form-about-yourself"></textarea>
+                                            <span *ngIf="abutMeWarning" class="glyphicon glyphicon-remove form-control-feedback"></span>
+                                             <span class="control-label" *ngIf="abutMeWarning">
+                                            You need to fill up this form
+                                           </span>
 				                        </div>
-                                        <div class="form-group row">
+
+                                        <div [ngClass]="{'form-group':true,'has-error has-feedback': passwordWarning}">
                                             <label for="password-input" class="form-label" id="label-password">Password:</label>
-                                                <input class="form-control" type="password" placeholder="Password..."  id="password-input">
+                                                <input #password class="form-control" type="password" placeholder="Password..."  id="password-input">
+                                                <span *ngIf="passwordWarning" class="glyphicon glyphicon-remove form-control-feedback"></span>
+                                                  <span class="control-label" *ngIf="passwordWarning">
+                                              You need to fill up this form
+                                                </span>
                                         </div>
-                                        <div class="form-group row">
+                                       
+                                        <div [ngClass]="{'form-group':true,'has-error has-feedback': passwordConfirmWarning || passwordValidation}">
                                             <label for="passwordConfirm-input" class="sr-only">Password</label>
-                                                <input class="form-control" type="password" placeholder="Confirm Password..."  id="passwordConfirm-input">
+                                                <input #passwordConfirm class="form-control" type="password" placeholder="Confirm Password..."  id="passwordConfirm-input">
+                                                <span *ngIf="passwordConfirmWarning" class="glyphicon glyphicon-remove form-control-feedback"></span>
+                                                  <span class="control-label" *ngIf="passwordConfirmWarning">
+                                            You need to fill up this form
+                                            </span>
+                                            <span class="control-label" *ngIf="passwordValidation">
+                                            You confirm password doesnt match with password, please repeat input !!! 
+                                            </span>
                                         </div>
-				                        <button class="btn btn-primary btn-lg btn-block router-link" routerLink="/login" routerLinkActive="active">Sign up</button>
+				                        <button class="btn btn-primary btn-lg btn-block router-link" routerLink="/registration" routerLinkActive="active"
+                                         (click)="addUser(firstName,lastName,username,city,country,date,email,aboutMe,password,passwordConfirm)">Sign up</button>
 				                    </form>
 			                    </div>
                         	</div>
@@ -71,7 +132,7 @@ import { Component } from '@angular/core';
             </div>
             <div class="modal-footer">
                 <div class="col-md-12">
-                <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+                <button class="btn router-link" data-dismiss="modal" routerLink="/login" routerLinkActive="active" aria-hidden="true">Cancel</button>
                 </div>	
             </div>
         </div>
@@ -79,8 +140,99 @@ import { Component } from '@angular/core';
     </div>
     `
 })
-export default class RegistrationWindow{
 
-    constructor(){
+
+
+export default class RegistrationWindow implements AfterContentChecked
+{
+    public firstNameWarning:boolean=false;
+    public lastNameWarning:boolean=false;
+    public usernameWarning:boolean=false;
+    public cityWarning:boolean=false;
+    public countryWarning:boolean=false;
+    public dateWarning:boolean=false;
+    public emailWarning:boolean=false;
+    public abutMeWarning:boolean=false;
+    public passwordWarning:boolean=false;
+    public passwordConfirmWarning:boolean=false;
+    public passwordValidation=false;
+
+    private userService: UserService;
+    private userAlreadyExistWarning:boolean;
+    
+    ngAfterContentChecked():void{this.userAlreadyExistWarning=this.userService.sameUserName();}
+    
+    constructor(userService: UserService){
+        this.userService = userService;
     }
+
+    addUser(firstName:HTMLInputElement,lastName:HTMLInputElement,username:HTMLInputElement,
+    city:HTMLInputElement,country:HTMLInputElement,date:HTMLInputElement,email:HTMLInputElement,
+    aboutMe:HTMLInputElement,password:HTMLInputElement,passwordConfirm:HTMLInputElement)
+    {
+        if(firstName.value === "")
+            this.firstNameWarning=true;
+        else 
+            this.firstNameWarning=false;
+        if(lastName.value === "")
+            this.lastNameWarning=true;
+        else 
+            this.lastNameWarning=false;
+        if(username.value === "")
+        {
+            this.usernameWarning=true;
+            this.userAlreadyExistWarning=false;
+        }
+        else 
+            this.usernameWarning=false;
+        if(city.value === "")
+            this.cityWarning=true;
+        else 
+            this.cityWarning=false;
+        if(country.value === "")
+            this.countryWarning=true;
+        else 
+            this.countryWarning=false;
+        if(date.value === "")
+            this.dateWarning=true;
+        else 
+            this.dateWarning=false;
+        if(email.value === "")
+            this.emailWarning=true;
+        else 
+            this.emailWarning=false;
+        if(aboutMe.value === "")
+            this.abutMeWarning=true;
+        else 
+            this.abutMeWarning=false;
+        if(password.value === "")
+            this.passwordWarning=true;
+        else 
+            this.passwordWarning=false;
+        if(passwordConfirm.value === "")
+            this.passwordConfirmWarning=true;
+        else
+        { 
+            this.passwordConfirmWarning=false;
+            if(password.value === passwordConfirm.value)
+                this.passwordValidation=false
+            else    
+                this.passwordValidation=true
+        }
+
+        if(!this.firstNameWarning && !this.lastNameWarning && !this.usernameWarning && !this.cityWarning && !this.countryWarning&& !this.dateWarning && !this.emailWarning && !this.passwordWarning &&!this.passwordConfirmWarning && !this.passwordValidation)
+        {
+             this.userService.addUser(firstName.value,lastName.value,username.value,city.value,country.value,date.value,email.value,aboutMe.value,password.value)
+        }
+        else
+        {}
+
+        
+      
+
+       
+    }
+
+
+
 }
--- app/services/UserService.ts
+++ app/services/UserService.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions , Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 import User from '../model/user';
 
 @Injectable()
 export default class UserService {
     public users: User[];
+    public userAlreadyExistWarning: boolean=false;
 
-    constructor(){
-        this.users = [ 
-            new User("Mario","Boban","mBoban","Croatia","Split","16.08.1994.","mboban@gmail.com","Jako sam cool!!!","andrija"),
-            new User("Toni","Buzov","tBuzov","Croatia","Kaštela","28.06.1994.","tonibuzov@gmail.com","Prejak sam !!!","fesb")
-            ];
-    }
+    private http : Http;
 
-    public addUser(firstName: string,lastName: string,twitterTag: string,country: string,city: string,dataOfBirth : string,email: string,
+    constructor(http:Http){
+      this.http = http;}
+
+    public addUser(firstName: string,lastName: string,userName: string,city: string,country: string,dateOfBirth : string,email: string,
     aboutMe: string, password: string)
     {
-        this.users.push(new User(firstName,lastName,twitterTag,country,city,dataOfBirth,email,aboutMe,password));
+        this.http.post("https://tweeter-api.herokuapp.com/signup",{ firstName: firstName, lastName: lastName,
+        userName:userName,  country:country, city:city, dateOfBirth:dateOfBirth, email:email, aboutSelf:aboutMe, password:password} )
+        .subscribe(
+            response => {console.log("Succesfully added user"),
+            this.userAlreadyExistWarning=false;
+            var newUser=response.json().user
+            console.log(newUser)},
+            error => {console.log("Error when adding user", error);
+            this.userAlreadyExistWarning=true; }
+        );
+    }
+
+    public sameUserName()
+    {
+        return this.userAlreadyExistWarning
     }
 }
